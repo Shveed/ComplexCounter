d2ed140fba2765bfcfa88365c0b92a0664fe298a
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-03-03 20:11:20 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -1,16 +1,18 @@
 #include "complex.h"
+#include "stdafx.h"
 #include <iostream>
+using namespace std;
 
 void printMenu()
 {
-	cout << "-------------------------------" << endl;
-	cout << "" << endl;
-	cout << " - " << endl;
-	cout << " -  " << endl;
-	cout << " - " << endl;
-	cout << " - " << endl;
-	cout << " - " << endl;
-	cout << "-------------------------------" << endl;
+	cout << "------------------------------------" << endl;
+	cout << "Welcome to program 'ComplexCounter' " << endl;
+	cout << "  First of all - choose operation " << endl;
+	cout << "+ - addiction " << endl;
+	cout << "- - substraction" << endl;
+	cout << "/ - division" << endl;
+	cout << "* - multiplication" << endl;
+	cout << "------------------------------------" << endl;
 }
 
 complex complexCreate(complex *c1)
@@ -20,6 +22,7 @@ complex complexCreate(complex *c1)
 	cin >> c1->imag;
 	return *c1;
 }
+
 char whichOp(char op)
 {
 	cout << "Enter operation: ";
@@ -27,17 +30,19 @@ char whichOp(char op)
 	return op;
 }
 
-complex complexAdd(complex c1, complex c2)
+complex complexAdd(complex *c1, complex *c2, complex *c3)
 {
-
+	c3->real = c1->real + c2->real;
+	c3->imag = c1->imag + c2->imag;
+	return *c3;
 }
 
-complex complexCreateFromPolar(double absVal, double arg);
-int complexToString(complex c, char buffer[], int size);
+//complex complexCreateFromPolar(double absVal, double arg);
+//int complexToString(complex c, char buffer[], int size);
 
-void printResult()
+void printResult(complex *c3)
 {
-
+	cout << "Result of operation is: " << &c3 << endl;
 }
 
 void printError()
