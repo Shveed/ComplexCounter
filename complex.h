7964ae8c9ff71f6658785a74ce570f1bb02e3b36
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-03-03 20:17:33 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -1,33 +1,33 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
-#include <iostream>
 #include <cmath>
 #include <string.h>
 #include "stdafx.h"
+#include <iostream>
 using namespace std;
 
 typedef struct complex *Complex;
 
 typedef struct complex{
-	int real;
-	int imag;
-}complex;
+	int real = 0;
+	int imag = 0;
+};
 
 void printMenu();
-void printResult();
+void printResult(complex *c3);
 
 char whichOp(char op);
 
-Complex complexCreate(complex c1);
+complex complexCreate(complex *c1);
 
-Complex complexAdd(double c1, double c2);
-Complex complexSub(double c1, double c2);
-Complex complexMult(double c1, double c2);
-Complex complexDiv(double c1, double c2);
+complex complexAdd(complex *c1, complex *c2, complex *c3);
+//complex complexSub(double c1, double c2);
+//complex complexMult(double c1, double c2);
+//complex complexDiv(double c1, double c2);
 void printError();
 
-Complex complexCreateFromPolar(double absVal, double arg);
-int complexToString(complex c, char buffer[], int size);
+//complex complexCreateFromPolar(double absVal, double arg);
+//int complexToString(complex c, char buffer[], int size);
 
 
 #endif COMPLEX_H
