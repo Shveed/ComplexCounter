575576a0872fd50e0911f6a6c251e936da150a1d
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-03-03 20:11:49 +0300

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,37 +1,33 @@
-#include "Complex.h"
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <cmath>
+#include <string.h>
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
-int main()
-{
-	char op = 0;
-	Complex c1;
-	complex c2;
-	complex c3;
+typedef struct complex *Complex;
 
+typedef struct complex{
+	int real = 0;
+	int imag = 0;
+};
 
-	printMenu();
-	whichOp(op);
-	switch(op)
-	{
-	case '+':
-		complexAdd();
-		break;
-	case '-':
-		complexSub();
-		break;
-	case '*':
-		complexMult();
-		break;
-	case '/':
-		complexDiv();
-		break;
-	default:
-		printError();
-	}
+void printMenu();
+void printResult(complex *c3);
+
+char whichOp(char op);
+
+complex complexCreate(complex *c1);
+
+complex complexAdd(complex *c1, complex *c2, complex *c3);
+//complex complexSub(double c1, double c2);
+//complex complexMult(double c1, double c2);
+//complex complexDiv(double c1, double c2);
+void printError();
+
+//complex complexCreateFromPolar(double absVal, double arg);
+//int complexToString(complex c, char buffer[], int size);
 
 
-
-
-
-
-}
+#endif COMPLEX_H
